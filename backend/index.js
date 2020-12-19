04cfb6d46bc03ab5ec8db64c1a5648a9e1c3524a
Abiona Michael <amabiona21@gmail.com>
2020-12-19 09:45:19 +0100

    prepare for deployment

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use('/api/memories', memoryRoute);
 app.use('/api/uploads', imageRoute);
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/upload/images')));
+app.use('/uploads', express.static(path.join(__dirname, 'backend/upload/images')));
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
