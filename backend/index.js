6714af4f331808313437e1f7ddb0f09e457e8ea0
Abiona Michael <amabiona21@gmail.com>
2020-12-19 09:14:39 +0100

    prepare for deployment

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,9 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/upload/images')));
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 } else {
     app.get('/', (req, res) => {
         res.send('Hello backend');
